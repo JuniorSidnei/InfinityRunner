2b8fc3e7d5f64d2d77255146ee0aa2239922935d
sidao <sidneicarrarojunio@gmail.com>
2022-04-05 20:56:46 -0300

    player life use correct value

--- Infinity-Runner/Assets/Scripts/Player/PlayerLife.cs
+++ Infinity-Runner/Assets/Scripts/Player/PlayerLife.cs
@@ -1,22 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using InfinityRunner.Managers;
+using InfinityRunner.Scriptables;
 using UnityEngine;
 
 namespace InfinityRunner.Player {
     
     public class PlayerLife : MonoBehaviour {
-        
-        public int Life = 5;
+
+        public PlayerStatus PlayerStatus;
         public LayerMask ObstacleLayer;
+
+        private int m_life;
         
+        private void Awake() {
+            m_life = PlayerStatus.Life;
+        }
+
         private void OnTriggerEnter2D(Collider2D other) {
             if (((1 << other.gameObject.layer) & ObstacleLayer) == 0) {
                 return;
             }
 
-            Life -= 1;
-            HudManager.Instance.UpdateLife(Life);
+            m_life -= 1;
+            HudManager.Instance.UpdateLife(m_life);
         }
     }
 }
