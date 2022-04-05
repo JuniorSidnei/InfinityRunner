24490b11deb2912086dc65338aee15bcd691aab9
sidao <sidneicarrarojunio@gmail.com>
2022-04-05 16:16:51 -0300

    options menus complete + add save system and upgrades/shop mechanics

--- Infinity-Runner/Assets/Scripts/Managers/OptionsManager.cs
+++ Infinity-Runner/Assets/Scripts/Managers/OptionsManager.cs
@@ -1,21 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-namespace InfinityRunner.Managers
-{
-
-
-    public class OptionsManager : MonoBehaviour
-    {
+namespace InfinityRunner.Managers {
+    
+    public class OptionsManager : MonoBehaviour {
+        
         public GameObject OptionsPanel;
+        public TMP_Dropdown Resolution;
+        public Button CredtisBtn;
+        public Button BackBtn;
+
+        private readonly Dictionary<int, Vector2Int> m_resolutionsInfo = new Dictionary<int, Vector2Int>();
+
+        private void Start() {
+            Resolution.onValueChanged.AddListener(delegate {
+                OnResolutionUpdate(Resolution);
+            });
+            
+            BackBtn.onClick.AddListener(Hide);
+            
+            m_resolutionsInfo[0] = new Vector2Int(1920, 1080);
+            m_resolutionsInfo[1] = new Vector2Int(1280, 720);
+        }
 
         public void Show() {
             OptionsPanel.SetActive(true);
         }
+        
+        private void Hide() {
+            OptionsPanel.SetActive(false);
+        }
 
         public void OnChangeFullscreen() {
             Screen.fullScreen = !Screen.fullScreen;
+        }
+
+        private void OnResolutionUpdate(TMP_Dropdown dropdown) {
+            var resolution = m_resolutionsInfo[dropdown.value];
+            Screen.SetResolution(resolution.x, resolution.y, Screen.fullScreen);
         }
     }
 }
